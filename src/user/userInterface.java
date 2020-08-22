002ac79ac0f39a5389711b66e68c5339d26badf7
Ananya Mukherjee <ananya.ece98@gmail.com>
2020-08-22 19:53:24 +0530

    Initial commit

---
+++ src/user/userInterface.java
@@ -0,0 +1,27 @@
+package user;
+import entity.Course;
+import entity.Professor;
+import entity.Student;
+import Operations.*;
+import java.util.*;
+public class userInterface {
+	Scanner sc = new Scanner(System.in);
+
+	public static void displayMenu() {
+
+		System.out.println("Menu");
+		System.out.println("1. Admin");
+		System.out.println("2. Professor");
+		System.out.println("3. Student");
+		System.out.println("4. Display Details");
+		System.out.println("5. Exit");
+		System.out.println("=========================================");
+	}
+	
+	public static boolean switcher(Connection con,)
+
+	public static void main(String[] args) {
+		
+	}
+
+}
